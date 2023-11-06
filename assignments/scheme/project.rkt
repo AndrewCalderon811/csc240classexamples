c7870bb665153498883a66e257fde6b0186b3e99
AndrewCalderon811 <111481804+AndrewCalderon811@users.noreply.github.com>
2023-11-06 13:56:38 -0700

    Update project.rkt

--- assignments/scheme/project.rkt
+++ assignments/scheme/project.rkt
@@ -14,3 +14,22 @@
 
 (define remove-dups
   (lambda (lst)
+    (if (> (length lst) 1)
+        (if (equal? (car lst) (car (cdr lst)))
+            (cons (car lst) (remove-dups (cddr lst)))
+            (cons (car lst) (remove-dups (cdr lst))))
+        (cons (car lst) '()))))
+
+
+    
+    
+
+(define deep-search
+  (lambda (lst X)
+    (if (null? lst)
+        #f
+        (if (eq? (car lst) X)
+             #t
+             (deep-search (cdr lst) X)))))
+    
+    
